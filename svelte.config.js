99c640b7f0872eb6551bbafec67e6d9bd9dc0dd9
Navid Zand <navidzand131@gmail.com>
2024-08-19 23:36:33 +0200

    fix: add cloudflare

--- svelte.config.js
+++ svelte.config.js
@@ -2,10 +2,10 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 
 // cloudflare 
-// import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-cloudflare';
 
 // static
-import adapter from '@sveltejs/adapter-static';
+// import adapter from '@sveltejs/adapter-static';
 
 // // auto
 // import adapter from '@sveltejs/adapter-auto';
@@ -28,24 +28,24 @@ const config = {
 
 	kit: {
 		// --------------------------------------------------- static
-		adapter: adapter({
-			fallback: 'index.html'
-		}),
+		// adapter: adapter({
+		// 	fallback: 'index.html'
+		// }),
 
 		// --------------------------------------------------- cloudflare 
-		// adapter: adapter({
-		// 	// See below for an explanation of these options
-		// 	routes: {
-		// 		include: ['/*'],
-		// 		exclude: ['<all>']
-		// 	},
-		// 	platformProxy: {
-		// 		configPath: 'wrangler.toml',
-		// 		environment: undefined,
-		// 		experimentalJsonConfig: false,
-		// 		persist: false
-		// 	}
-		// }),
+		adapter: adapter({
+			// See below for an explanation of these options
+			routes: {
+				include: ['/*'],
+				exclude: ['<all>']
+			},
+			platformProxy: {
+				configPath: 'wrangler.toml',
+				environment: undefined,
+				experimentalJsonConfig: false,
+				persist: false
+			}
+		}),
 
 		// // --------------------------------------------------- auto
 		// adapter: adapter(),
